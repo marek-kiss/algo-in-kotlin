4d385ed47d221f66ad4855d3c38ebbf8b029e9fe
marek-kiss <25489763+marek-kiss@users.noreply.github.com>
2021-11-22 22:00:39 +0100

    Add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "sk.marek-kiss"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.21.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
