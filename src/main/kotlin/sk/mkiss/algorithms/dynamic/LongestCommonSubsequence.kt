cd1f63c4094dc9450820bd6c295a5deb5a1c8a6f
marek-kiss <25489763+marek-kiss@users.noreply.github.com>
2022-02-06 10:58:31 +0100

    Add solution for the longest common subsequence

--- src/main/kotlin/sk/mkiss/algorithms/dynamic/LongestCommonSubsequence.kt
+++ src/main/kotlin/sk/mkiss/algorithms/dynamic/LongestCommonSubsequence.kt
@@ -5,20 +5,49 @@ import kotlin.math.max
 object LongestCommonSubsequence {
 
     fun getSizeOfLongestCommonSubsequence(a: List<Int>, b: List<Int>): Int {
-        val d = mutableMapOf<Pair<Int, Int>, Int>()
-        lcs(a, b, a.size, b.size, d)
-        return d[Pair(a.size, b.size)]!!
+        val lcsMemory = mutableMapOf<Pair<Int, Int>, Int>()
+        lcs(a, b, a.size, b.size, lcsMemory)
+        return lcsMemory[a.size, b.size]!!
     }
 
-    private fun lcs(a: List<Int>, b: List<Int>, aSize: Int, bSize: Int, d: MutableMap<Pair<Int, Int>, Int>): Int {
-        return d.getOrPut(Pair(aSize, bSize)) {
+    fun getLongestCommonSubsequence(a: List<Int>, b: List<Int>): List<Int> {
+        val lcsMemory = mutableMapOf<Pair<Int, Int>, Int>()
+        lcs(a, b, a.size, b.size, lcsMemory)
+        return backtrack(a, b, lcsMemory)
+    }
+
+    private fun lcs(a: List<Int>, b: List<Int>, aSize: Int, bSize: Int, memory: MutableMap<Pair<Int, Int>, Int>): Int {
+        return memory.getOrPut(Pair(aSize, bSize)) {
             if (aSize <= 0 || bSize <= 0) {
                 0
             } else if (a[aSize - 1] != b[bSize - 1]) {
-                max(lcs(a, b, aSize - 1, bSize, d), lcs(a, b, aSize, bSize - 1, d))
+                max(lcs(a, b, aSize - 1, bSize, memory), lcs(a, b, aSize, bSize - 1, memory))
             } else {
-                lcs(a, b, aSize - 1, bSize - 1, d) + 1
+                lcs(a, b, aSize - 1, bSize - 1, memory) + 1
             }
         }
     }
+
+    private fun backtrack(a: List<Int>, b: List<Int>, memory: Map<Pair<Int, Int>, Int>): List<Int> {
+        val lcs = mutableListOf<Int>()
+
+        var aI = a.size
+        var bI = b.size
+
+        while (aI > 0 && bI > 0) {
+            when (memory[aI, bI]) {
+                memory[aI, bI - 1] -> bI -= 1
+                memory[aI - 1, bI] -> aI -= 1
+                else -> {
+                    aI -= 1
+                    bI -= 1
+                    if (a[aI] == b[bI]) lcs.add(a[aI])
+                }
+            }
+        }
+
+        return lcs.reversed()
+    }
+
+    private operator fun <T> Map<Pair<Int, Int>, T>.get(x: Int, y: Int): T? = get(Pair(x, y))
 }
--- src/test/kotlin/sk/mkiss/algorithms/dynamic/LongestCommonSubsequenceTest.kt
+++ src/test/kotlin/sk/mkiss/algorithms/dynamic/LongestCommonSubsequenceTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 internal class LongestCommonSubsequenceTest {
 
     @Test
-    fun `should handle an empty list`() {
+    fun `getSizeOfLongestCommonSubsequence - should handle an empty list`() {
         assertThat(
             LongestCommonSubsequence.getSizeOfLongestCommonSubsequence(
                 emptyList(),
@@ -16,19 +16,34 @@ internal class LongestCommonSubsequenceTest {
     }
 
     @Test
-    fun `should handle both lists empty`() {
-        assertThat(
-            LongestCommonSubsequence.getSizeOfLongestCommonSubsequence(emptyList(), emptyList())
-        ).isEqualTo(0)
+    fun `getSizeOfLongestCommonSubsequence - should handle both lists empty`() {
+        assertThat(LongestCommonSubsequence.getSizeOfLongestCommonSubsequence(emptyList(), emptyList())).isEqualTo(0)
     }
 
     @Test
-    fun `should return the size of the longest common subsequence`() {
+    fun `getSizeOfLongestCommonSubsequence - should return the correct size`() {
         assertThat(
             LongestCommonSubsequence.getSizeOfLongestCommonSubsequence(
                 listOf(1, 2, 1, 3, 1, 4),
                 listOf(3, 1, 1, 1, 3, 1, 2),
             )
         ).isEqualTo(4)
+    }
+
+    @Test
+    fun `getLongestCommonSubsequence - handles empty list`() {
+        assertThat(LongestCommonSubsequence.getLongestCommonSubsequence(emptyList(), listOf(3))).isEmpty()
+    }
+
+    @Test
+    fun `getLongestCommonSubsequence - returns LCS`() {
+        assertThat(
+            LongestCommonSubsequence.getLongestCommonSubsequence(
+                listOf(1, 2, 1, 3, 1, 4, 5, 1, 1),
+                listOf(3, 1, 1, 1, 3, 1, 2, 4, 1, 1, 1, 1, 5),
+            )
+        ).isEqualTo(
+            listOf(1, 1, 3, 1, 4, 1, 1)
+        )
     }
 }
